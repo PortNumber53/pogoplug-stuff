298025e49504ec76db0063612f45c9f560ebd026
Maurício S Otta <mauricio.otta@in.sert.co.in>
2013-03-22 00:10:40 -0700

    Uppercased the params names [DAYS,FILTER]

--- templates/clean_up_backup.sh
+++ templates/clean_up_backup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-COUNT=`/usr/bin/find $filter -maxdepth 0 -type d -mtime +$days | /usr/bin/wc -l`
+COUNT=`/usr/bin/find $FILTER -maxdepth 0 -type d -mtime +$DAYS | /usr/bin/wc -l`
 MINDIRS=$mininum
 
 if [[ $COUNT -gt $MINDIRS ]]
         then
-		/usr/bin/find $filter -maxdepth 0 -type d -mtime +$days
+		/usr/bin/find $FILTER -maxdepth 0 -type d -mtime +$DAYS
 		#exit
-		for i in `/usr/bin/find $filter -maxdepth 0 -type d -mtime +$days`; do
+		for i in `/usr/bin/find $FILTER -maxdepth 0 -type d -mtime +$DAYS`; do
 			echo -e "Deleting $i";
 			rm -rfv $i;
 		done
