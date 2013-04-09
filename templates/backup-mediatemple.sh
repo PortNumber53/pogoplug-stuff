a8df0eed92a758fdc206f83219853caca0a93c66
Mauricio S Otta <mauricio@portnumber53.com>
2013-04-09 02:45:55 -0700

    Update backup-mediatemple.sh
    Removed $pid dependency (let's use flock on crontab)

--- templates/backup-mediatemple.sh
+++ templates/backup-mediatemple.sh
@@ -8,31 +8,20 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $DIR/../custom/backup-variables.sh
 
-pid=/tmp/$PROJECT.pid
 TODAY_FOLDER=$backup_folder/`date +%Y-%m-%d --date "0 day ago"`/$PROJECT
 YESTERDAY_FOLDER=$backup_folder/`date +%Y-%m-%d --date "1 day ago"`/$PROJECT
 
-if [ -e $pid ]
-then
-	echo "$PROJECT Still running..."
-else
-	touch $pid
-
-	if [ ! -d $TODAY_FOLDER ]; then
-		mkdir -p $TODAY_FOLDER
-		echo "copying..."
-		cp -alv $YESTERDAY_FOLDER/. $TODAY_FOLDER
-	fi
-	if [ ! -d $TODAY_FOLDER/data ]; then
-		mkdir -p $TODAY_FOLDER/data
-	fi
-	if [ ! -d $TODAY_FOLDER/domains ]; then
-		mkdir -p $TODAY_FOLDER/domains
-	fi
-
-	rsync --bwlimit=$DOWNLOAD_SPEED --force --no-p --progress --delete-before --exclude=tmp/sess_ -laze ssh $PROJECT:~/../../data/ $TODAY_FOLDER/data 1> /tmp/$PROJECT.log 2>&1
-	rsync --bwlimit=$DOWNLOAD_SPEED --force --no-p --progress --delete-before -laze ssh $PROJECT:~/../../domains/ $TODAY_FOLDER/domains 1> /tmp/$PROJECT.log 2>&1
-
-	rm $pid
+if [ ! -d $TODAY_FOLDER ]; then
+	mkdir -p $TODAY_FOLDER
+	echo "copying..."
+	cp -alv $YESTERDAY_FOLDER/. $TODAY_FOLDER
+fi
+if [ ! -d $TODAY_FOLDER/data ]; then
+	mkdir -p $TODAY_FOLDER/data
+fi
+if [ ! -d $TODAY_FOLDER/domains ]; then
+	mkdir -p $TODAY_FOLDER/domains
 fi
 
+rsync --bwlimit=$DOWNLOAD_SPEED --force --no-p --progress --delete-before --exclude=tmp/sess_ -laze ssh $PROJECT:~/../../data/ $TODAY_FOLDER/data 1> /tmp/$PROJECT.log 2>&1
+rsync --bwlimit=$DOWNLOAD_SPEED --force --no-p --progress --delete-before -laze ssh $PROJECT:~/../../domains/ $TODAY_FOLDER/domains 1> /tmp/$PROJECT.log 2>&1
