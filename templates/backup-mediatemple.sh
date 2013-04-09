b875dbc3fa56af879a9a56c1f8573b95f6db5bf9
Mauricio S Otta <mauricio@portnumber53.com>
2013-04-09 02:45:01 -0700

    Update backup-mediatemple.sh
    Fixed typo

--- templates/backup-mediatemple.sh
+++ templates/backup-mediatemple.sh
@@ -30,7 +30,7 @@ else
 		mkdir -p $TODAY_FOLDER/domains
 	fi
 
-	rsync --bwlimit=$DOWNLOAD_SPEED --force --no-p --progress --delete-before --exclude=tmp/sess_ --laze ssh $PROJECT:~/../../data/ $TODAY_FOLDER/data 1> /tmp/$PROJECT.log 2>&1
+	rsync --bwlimit=$DOWNLOAD_SPEED --force --no-p --progress --delete-before --exclude=tmp/sess_ -laze ssh $PROJECT:~/../../data/ $TODAY_FOLDER/data 1> /tmp/$PROJECT.log 2>&1
 	rsync --bwlimit=$DOWNLOAD_SPEED --force --no-p --progress --delete-before -laze ssh $PROJECT:~/../../domains/ $TODAY_FOLDER/domains 1> /tmp/$PROJECT.log 2>&1
 
 	rm $pid
