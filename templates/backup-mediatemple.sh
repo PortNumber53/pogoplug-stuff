85c99653070505fdf9a538b2af2d37c62797f173
Mauricio S Otta <mauricio.otta@in.sert.co.in>
2013-06-30 12:26:38 -0700

    Use flock to prevent multiple instances of backup-mediatemple.sh

--- templates/backup-mediatemple.sh
+++ templates/backup-mediatemple.sh
@@ -5,23 +5,34 @@
 #  THIS FILE IS CALLED BY OTHERS
 #
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-source $DIR/../custom/backup-variables.sh
+# Makes sure we exit if flock fails.
+set -e
 
-TODAY_FOLDER=$backup_folder/`date +%Y-%m-%d --date "0 day ago"`/$PROJECT
-YESTERDAY_FOLDER=$backup_folder/`date +%Y-%m-%d --date "1 day ago"`/$PROJECT
+(
+  # Wait for lock on /var/lock/.myscript.exclusivelock (fd 200) for 10 seconds
+  flock -x -w 10 200
 
-if [ ! -d $TODAY_FOLDER ]; then
-	mkdir -p $TODAY_FOLDER
-	echo "copying..."
-	cp -alv $YESTERDAY_FOLDER/. $TODAY_FOLDER
-fi
-if [ ! -d $TODAY_FOLDER/data ]; then
-	mkdir -p $TODAY_FOLDER/data
-fi
-if [ ! -d $TODAY_FOLDER/domains ]; then
-	mkdir -p $TODAY_FOLDER/domains
-fi
 
-rsync --bwlimit=$DOWNLOAD_SPEED --force --no-p --progress --delete-before --exclude=tmp/sess_ -laze ssh $PROJECT:~/../../data/ $TODAY_FOLDER/data 1> /tmp/$PROJECT.log 2>&1
-rsync --bwlimit=$DOWNLOAD_SPEED --force --no-p --progress --delete-before -laze ssh $PROJECT:~/../../domains/ $TODAY_FOLDER/domains 1> /tmp/$PROJECT.log 2>&1
+  DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+  source $DIR/../custom/backup-variables.sh
+
+  TODAY_FOLDER=$backup_folder/`date +%Y-%m-%d --date "0 day ago"`/$PROJECT
+  YESTERDAY_FOLDER=$backup_folder/`date +%Y-%m-%d --date "1 day ago"`/$PROJECT
+
+  if [ ! -d $TODAY_FOLDER ]; then
+    mkdir -p $TODAY_FOLDER
+    echo "copying..."
+    cp -alv $YESTERDAY_FOLDER/. $TODAY_FOLDER
+  fi
+  if [ ! -d $TODAY_FOLDER/data ]; then
+    mkdir -p $TODAY_FOLDER/data
+  fi
+  if [ ! -d $TODAY_FOLDER/domains ]; then
+    mkdir -p $TODAY_FOLDER/domains
+  fi
+
+  rsync --bwlimit=$DOWNLOAD_SPEED --force --no-p --progress --delete-before --exclude=tmp/sess_ -laze ssh $PROJECT:~/../../data/ $TODAY_FOLDER/data 1> /tmp/$PROJECT.log 2>&1
+  rsync --bwlimit=$DOWNLOAD_SPEED --force --no-p --progress --delete-before -laze ssh $PROJECT:~/../../domains/ $TODAY_FOLDER/domains 1> /tmp/$PROJECT.log 2>&1
+
+) 200>/var/lock/.$PROJECT.exclusivelock
+
